906a9fb83068a9c1c54f1e7657db5edbacf75f1b
Victor Rocha <victorhugomdarocha@gmail.com>
2026-03-20 13:58:58 -0300

    adicionei o primeiro exercicio

---
+++ ex001.py
@@ -0,0 +1,6 @@
+with open('frutas.txt', 'w', encoding='utf-8') as arquivo:
+    arquivo.write('Maçã')
+    arquivo.write('Banana')
+    arquivo.write('Laranja')
+    arquivo.write('Melão')
+    arquivo.write('Melancia')
